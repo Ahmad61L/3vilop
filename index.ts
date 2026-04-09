cd906138c7a8598de1c859d726952ecb4e85ee46
Ahmad61L <157296898+Ahmad61L@users.noreply.github.com>
2026-04-09 22:56:09 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot ist online als ${client.user?.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
